e67fc3eb12d5ebec903dfb4cdffaa52943c9ee38
Lucca D'Amico Diniz <lucca.damico@hotmail.com>
2021-09-06 16:45:37 -0400

    Criado o layout e floating action button, para adicionar tarefas.

---
+++ lib/Home.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  List _listaTarefas = ["Ir ao mercado", "Estudar", "Exercício do dia"];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Lista de tarefas"),
+        backgroundColor: Colors.purple,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        backgroundColor: Colors.purple,
+        onPressed: (){
+          
+          showDialog(
+              context: context,
+              builder: (context){
+
+                return AlertDialog(
+                  title: Text("Adicionar Tarefa"),
+                  content: TextField(
+                    decoration: InputDecoration(
+                      labelText: "Digite sua tarefa"
+                    ),
+                    onChanged: (text){
+
+                    },
+                  ),
+                  actions: [
+                    FlatButton(
+                        onPressed: () => Navigator.pop(context),
+                        child: Text("Cancelar")
+                    ),
+                    FlatButton(
+                        onPressed: (){
+
+                          Navigator.pop(context);
+                        },
+                        child: Text("Salvar")
+                    )
+                  ],
+                );
+
+              }
+          );
+          
+        },
+      ),
+      body: Column(
+        children: [
+          Expanded(
+              child: ListView.builder(
+                itemCount: _listaTarefas.length,
+                itemBuilder:(context, index){
+                  return ListTile(
+                    title: Text(_listaTarefas[index]),
+                  );
+                } ,
+              )
+          )
+        ],
+      ),
+    );
+  }
+}
