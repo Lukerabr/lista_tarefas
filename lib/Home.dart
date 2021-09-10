247ea24ec282b792f81edfdaa07b17476bad8326
Lucca D'Amico Diniz <lucca.damico@hotmail.com>
2021-09-10 18:37:02 -0400

    Salvando os dados no celular.

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:async';
+import 'dart:convert';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -9,10 +13,30 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
 
-  List _listaTarefas = ["Ir ao mercado", "Estudar", "Exercício do dia"];
+  List _listaTarefas = [];
+
+  _salvarArquivo() async {
+
+    final diretorio = (await getApplicationDocumentsDirectory()).path;
+    var arquivo = File("$diretorio/dados.json");
+
+    //Criar dados
+    Map<String, dynamic> tarefa = Map();
+    tarefa["titulo"] = "ir ao mercado";
+    tarefa["realizada"] = false;
+    _listaTarefas.add(tarefa);
+
+    String dados = json.encode(_listaTarefas);
+    arquivo.writeAsString(dados);
+    //print("Caminho: " + diretorio.path);
+
+  }
 
   @override
   Widget build(BuildContext context) {
+
+    _salvarArquivo();
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Lista de tarefas"),
